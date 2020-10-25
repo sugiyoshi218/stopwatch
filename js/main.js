36d504a72823167970d05dc0c96b45b9738361e1
sugiyoshi218 <72730557+sugiyoshi218@users.noreply.github.com>
2020-10-25 14:15:02 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,65 +1,69 @@
 'use strict';
 
-const timer = document.getElementById('timer');
-const start = document.getElementById('start');
-const stop = document.getElementById('stop');
-const reset = document.getElementById('reset');
+(()=>{
 
-let startTime;
-let timeoutId;
-let elapsedTime = 0;
+    const timer = document.getElementById('timer');
+    const start = document.getElementById('start');
+    const stop = document.getElementById('stop');
+    const reset = document.getElementById('reset');
 
-function countUp(){
-    const d = new Date(Date.now()-startTime + elapsedTime);
-    const m = String(d.getMinutes()).padStart(2,'0');
-    const s = String(d.getSeconds()).padStart(2,'0');
-    const ms = String(d.getMilliseconds()).padStart(3,'0');
-    timer.textContent=`${m}.${s}.${ms}`;
+    let startTime;
+    let timeoutId;
+    let elapsedTime = 0;
 
-    timeoutId=setTimeout(()=>{
-        countUp();
-    },10);
-}
+    function countUp(){
+        const d = new Date(Date.now()-startTime + elapsedTime);
+        const m = String(d.getMinutes()).padStart(2,'0');
+        const s = String(d.getSeconds()).padStart(2,'0');
+        const ms = String(d.getMilliseconds()).padStart(3,'0');
+        timer.textContent=`${m}.${s}.${ms}`;
 
-function setInitial(){
-    start.classList.remove('inactive');
-    stop.classList.add('inactive');
-    reset.classList.add('inactive');
-}
-function setRunning(){
-    start.classList.add('inactive');
-    stop.classList.remove('inactive');
-    reset.classList.add('inactive');
-}
-function setStopped(){
-    start.classList.remove('inactive');
-    stop.classList.add('inactive');
-    reset.classList.remove('inactive');
-}
-
-setInitial();
-
-start.addEventListener('click',()=>{
-    if(start.classList.contains('inactive')===true){
-        return;
+        timeoutId=setTimeout(()=>{
+            countUp();
+        },10);
     }
-    setRunning()
-    startTime = Date.now();
-    countUp();
-});
-stop.addEventListener('click',()=>{
-    if(stop.classList.contains('inactive')===true){
-        return;
+
+    function setInitial(){
+        start.classList.remove('inactive');
+        stop.classList.add('inactive');
+        reset.classList.add('inactive');
     }
-    setStopped();
-    clearTimeout(timeoutId);
-    elapsedTime += Date.now()-startTime;
-});
-reset.addEventListener('click',()=>{
-    if(reset.classList.contains('inactive')===true){
-        return;
+    function setRunning(){
+        start.classList.add('inactive');
+        stop.classList.remove('inactive');
+        reset.classList.add('inactive');
     }
+    function setStopped(){
+        start.classList.remove('inactive');
+        stop.classList.add('inactive');
+        reset.classList.remove('inactive');
+    }
+
     setInitial();
-    timer.textContent='00.00.000'
-    elapsedTime = 0;
-});
+
+    start.addEventListener('click',()=>{
+        if(start.classList.contains('inactive')===true){
+            return;
+        }
+        setRunning()
+        startTime = Date.now();
+        countUp();
+    });
+    stop.addEventListener('click',()=>{
+        if(stop.classList.contains('inactive')===true){
+            return;
+        }
+        setStopped();
+        clearTimeout(timeoutId);
+        elapsedTime += Date.now()-startTime;
+    });
+    reset.addEventListener('click',()=>{
+        if(reset.classList.contains('inactive')===true){
+            return;
+        }
+        setInitial();
+        timer.textContent='00.00.000'
+        elapsedTime = 0;
+    });
+    
+})();
